3b33e893d81817e0514c530792074050b287227e
ChiriloviciDaniel <79525784+ChiriloviciDaniel@users.noreply.github.com>
2021-02-23 17:01:24 +0200

    Create Project.c

---
+++ Project.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<conio.h>
+
+/*Se dă un tablou de N elemente aleatorii. Să se sorteze tabloul prin algoritmii Bubblesort,
+Shellsort și Quicksort și să se compare timpul de execuție pentru cele 3 metode.*/
+
+
+
+void main()
+{
+
+get_ch();
+return 0;
+}
